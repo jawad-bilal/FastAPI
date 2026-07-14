260df307c71a0fe129e2d9738ead08caa851632d
jawad-bilal <jawadbilal202006@gmail.com>
2026-07-14 17:20:32 +0500

    Add Procfile for deployment and configure CORS middleware in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, middleware
 from app.routes import user , post , practice, vote
 from .routes import auth
 from . import models
 from app.databaseORM import engine
+from fastapi.middleware.cors import CORSMiddleware
 # from pydantic import BaseModel
 # from random import randrange
 # from app.database import conn, cursor
@@ -15,7 +16,25 @@ from app.databaseORM import engine
 
 models.Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080", 
+    "http://www.google.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],) 
+    
+    
+    
+    
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
